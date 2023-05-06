a63dd0795ed35868820b451523e06b94bddb6a1b
David_Holliday <dhholliday@icloud.com>
2023-05-06 22:27:40 +0100

    Create a flood severity selector.

--- src/components/FloodForm.js
+++ src/components/FloodForm.js
@@ -1,6 +1,29 @@
-const FloodForm = () => {
+import classes from "./FloodForm.module.css";
+import { useState } from "react";
 
-    return(<h1>Flood Form goes here!</h1>)
+const FloodForm = () => {
+  const [floodValue, setFloodValue] = useState(4);
+
+  const changeHandler = event => {
+    setFloodValue(event.target.value);
+  };
+
+  return (
+    <div className={classes.all}>
+      <form>
+        <label for="number-input">What Severity do you want to see?</label>
+        <input
+          id="number-input"
+          type="number"
+          value={floodValue}
+          onChange={changeHandler}
+          min="1"
+          max="4"
+        />
+        <p>{floodValue}</p>
+      </form>
+    </div>
+  );
 };
 
 export default FloodForm;
